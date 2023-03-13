9b5b61a4844aecf32755c3f26432429118b3f860
EstherRH00 <eruanohortoneda@gmail.com>
2023-03-13 17:46:07 +0100

    Refactored computeShadow

--- Model/Rendering/ShadingStrategy.hh
+++ Model/Rendering/ShadingStrategy.hh
@@ -3,6 +3,7 @@
 #include "Model/Modelling/Scene.hh"
 #include "Model/Modelling/Lights/LightFactory.hh"
 
+#define EPSILON 0.01f
 
 class ShadingStrategy {
  public:
@@ -11,8 +12,28 @@ class ShadingStrategy {
         return vec3(0.0, 0.0, 0.0);
     };
 
+
     // FASE 2: Calcula si el punt "point" és a l'ombra segons si el flag està activat o no
-    float computeShadow(shared_ptr<Scene> scene, shared_ptr<Light> light, vec3 point);
+    float computeShadow(shared_ptr<Scene> scene, shared_ptr<Light> light, vec3 point){
+        vec3 L = light->vectorL(point);
+        Ray shadowRay(point, L);
+        HitInfo shadowInfo;
+        float maxDist = length(L);
+        if (scene->hit(shadowRay, EPSILON, maxDist, shadowInfo)) {
+            // Point is in shadow
+            return 0.0f;
+        } else {
+            // Point is not in shadow
+            computeAttenuation(light, point);
+        }
+    }
 
     virtual ~ShadingStrategy() {};
+
+private:
+
+    float computeAttenuation(shared_ptr<Light> light, vec3 point){
+        float attenuation = 1.0f / light->attenuation(point);
+        return attenuation;
+    }
 };
