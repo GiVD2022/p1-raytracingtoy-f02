691aa642e404808f694074d04d540455cf420854
EstherRH00 <eruanohortoneda@gmail.com>
2023-03-20 17:32:33 +0100

    Fixed transp for recursion

--- Model/Modelling/Materials/Transparent.cpp
+++ Model/Modelling/Materials/Transparent.cpp
@@ -21,17 +21,23 @@ Transparent::~Transparent()
 }
 
 bool Transparent::scatter(const Ray& r_in, const HitInfo& rec, vec3& color, Ray & r_out) const  {
-    float mu_i = 1.0f; //Assumim que el medi es el buit (veure email)
+    float mu_i = 1.0f; //Assumim que el medi es el buit
     float mu_it = mu_i / mu_t;
 
     vec3  incident = normalize(r_in.getDirection());
     vec3 normal = normalize(rec.normal);
 
+    // Si va de dins a fora, canviem la normal i el quocient
+    if(glm::dot(r_in.getDirection(), normal) > 0){
+        normal = -normal;
+        mu_it = 1.f/mu_it;
+    }
+
     // Calcular un unic raig transmes
     vec3 vec_out = glm::refract(incident, normal, mu_it);
     r_out = Ray(rec.p, vec_out);
 
-    // Si reflexio interna: ks; si no, kt
+    // Si reflexio interna: ks; si no, k
 
     //Reflexio interna si 1 - mu_it^2 * (sin^2(angle incidencia)) < 0
     // || u x v || = |u| * |v| * sin(uv)
