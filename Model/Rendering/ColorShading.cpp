9bcea6baf7d4df4076eb5f82574877360b0574b7
EstherRH00 <eruanohortoneda@gmail.com>
2023-03-25 15:23:22 +0100

    fixed typos

--- Model/Rendering/ColorShading.cpp
+++ Model/Rendering/ColorShading.cpp
@@ -2,6 +2,5 @@
 
 
 vec3 ColorShading::shading(shared_ptr<Scene> scene, HitInfo& info, vector<shared_ptr<Light>> lights, vec3 lookFrom, vec3 globalLight) {
-    //Retorna kd si no es texture
-    return (info.mat_ptr->getDiffuse(info.uv));
+    return (info.mat_ptr->Kd);
 }
--- Model/Rendering/ColorShadow.cpp
+++ Model/Rendering/ColorShadow.cpp
@@ -2,6 +2,5 @@
 
 vec3 ColorShadow::shading(shared_ptr<Scene> scene, HitInfo& info, vector<shared_ptr<Light>> lights, vec3 lookFrom, vec3 globalLight) {
     // TODO: Fase 2: Canviar per a considerar les ombres en aquest shading
-    // (info.mat_ptr->getDiffuse(info.uv)) retorna kd si no es texture
-    return vec3(1.0)-(info.mat_ptr->getDiffuse(info.uv));
+    return vec3(1.0)-(info.mat_ptr->Kd);
 }
