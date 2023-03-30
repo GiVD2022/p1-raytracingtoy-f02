c110657ea21252110da3503a2cd40db29126c693
phernama21 <pau.hm8@gmail.com>
2023-03-30 18:38:08 +0200

    normal and color shadow fix

--- Model/Rendering/ColorShadow.cpp
+++ Model/Rendering/ColorShadow.cpp
@@ -1,18 +1,10 @@
 #include "ColorShadow.hh"
 
 vec3 ColorShadow::shading(shared_ptr<Scene> scene, HitInfo& info, vector<shared_ptr<Light>> lights, vec3 lookFrom, vec3 globalLight) {
-    vec3 lightDiffuse = vec3(0.0);
+    vec3 color = info.mat_ptr->Kd;
     for (const auto& light : lights) {
-        // Calculate the diffuse component
-        vec3 L = light->vectorL(info.p); //normalized
-        vec3 N = info.normal;
-        float dotLN = dot(L, N);
-
-        // Check if the point is in shadow
         float shadowFactor = computeShadow(scene, light, info.p);
-        lightDiffuse += (info.mat_ptr->Kd * light->getId() * std::max(dotLN, 0.0f) * shadowFactor);
-
+        color *= shadowFactor;
     }
-    vec3 color = lightDiffuse;
     return color;
 }
--- Model/Rendering/NormalShadow.cpp
+++ Model/Rendering/NormalShadow.cpp
@@ -1,22 +1,12 @@
 #include "NormalShadow.hh"
 
 vec3 NormalShadow::shading(shared_ptr<Scene> scene, HitInfo& info, vector<shared_ptr<Light>> lights, vec3 lookFrom, vec3 globalLight) {
-    vec3 lightDiffuse = vec3(0.0);
     vec3 N = normalize(info.normal);
-    N = 0.5f *vec3(N.x+ 1.0f, N.y+ 1.0f, N.z+ 1.0f);
+    vec3 color = 0.5f *vec3(N.x+ 1.0f, N.y+ 1.0f, N.z+ 1.0f);
     for (const auto& light : lights) {
-
-        // Calculate the diffuse component
-        vec3 L = light->vectorL(info.p); //normalized
-        vec3 N = info.normal;
-        float dotLN = dot(L, N);
-
-        // Check if the point is in shadow
         float shadowFactor = computeShadow(scene, light, info.p);
-
-        lightDiffuse += (N * light->getId() * std::max(dotLN, 0.0f) * shadowFactor);
+        color*=shadowFactor;
 
     }
-    vec3 color = lightDiffuse;
     return color;
 }
